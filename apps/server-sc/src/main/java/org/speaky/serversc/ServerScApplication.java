d03cfb0abeb5ef1e6c19b205f4a402c5c52bbc85
sjkim1070 <sjkim1070@gmail.com>
2026-01-22 13:31:26 +0900

    initial commit

---
+++ apps/server-sc/src/main/java/org/speaky/serversc/ServerScApplication.java
@@ -0,0 +1,13 @@
+package org.speaky.serversc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerScApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerScApplication.class, args);
+    }
+
+}
---
+++ apps/server-sc/src/test/java/org/speaky/serversc/ServerScApplicationTests.java
@@ -0,0 +1,13 @@
+package org.speaky.serversc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerScApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
