a4ff26fbed43d8c5a51ad453803668becc6c3afb
sjkim1070 <sjkim1070@gmail.com>
2026-01-27 13:32:03 +0900

    feat : CreateSessionReqeust

---
+++ apps/server-sc/src/main/java/org/speaky/serversc/dto/CreateSessionRequest.java
@@ -0,0 +1,32 @@
+package org.speaky.serversc.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+/**
+ * 세션 생성 요청 DTO
+ */
+@Data
+public class CreateSessionRequest {
+    
+    /**
+     * 호스트(방송자) 사용자 ID
+     */
+    @NotNull(message = "Host user ID is required")
+    private Long hostUserId;
+    
+    /**
+     * 음성 모델 ID
+     */
+    @NotNull(message = "Voice model ID is required")
+    private Long voiceModelId;
+    
+    /**
+     * 방송 제목
+     */
+    @NotBlank(message = "Title is required")
+    @Size(max = 50, message = "Title must be less than 50 characters")
+    private String title;
+}
