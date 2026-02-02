88ef58ae6257ae3f5e4f627b2945e8d560cebad1
DESKTOP-6OO0P6F\SSAFY <kwk000530@naver.com>
2026-02-02 10:28:23 +0900

    fix:디버깅용 코드 삭제

--- apps/client-web/src/features/auth/api/login.ts
+++ apps/client-web/src/features/auth/api/login.ts
@@ -22,7 +22,6 @@ export async function login(req: LoginRequest) {
   if (!res.ok) throw new Error('로그인 실패');
 
   const responseBody = (await res.json()) as LoginResponse;
-  console.log('Login Response Data:', responseBody); // 디버깅용 로그
 
   if (!responseBody.success || !responseBody.data) {
     throw new Error(responseBody.error || '로그인 응답 형식이 올바르지 않습니다.');
