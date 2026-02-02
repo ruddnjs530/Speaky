ac2eee2528eb5d89badc8bdab1611dcd45d0a9d4
DESKTOP-6OO0P6F\SSAFY <kwk000530@naver.com>
2026-02-02 10:16:53 +0900

    fix: 로그인 응답 처리 및 세션 생성 요청 수정

--- apps/server-sc/src/main/java/org/speaky/serversc/dto/CreateSessionRequest.java
+++ apps/server-sc/src/main/java/org/speaky/serversc/dto/CreateSessionRequest.java
@@ -10,26 +10,26 @@ import lombok.Data;
  */
 @Data
 public class CreateSessionRequest {
-    
+
     /**
      * 호스트(방송자) 사용자 ID
      * TODO: JWT에서 추출하도록 변경 예정 (현재는 호환성을 위해 유지)
      */
     @NotNull(message = "Host user ID is required")
     private Long hostUserId;
-    
+
     /**
      * 채널 ID (Optional - 서버에서 자동 생성 가능)
      * 예: "ch_user_faker"
      */
     private String channelId;
-    
+
     /**
      * 음성 모델 ID (Optional)
      * 프론트엔드 스펙: voiceModelID
      */
     private Long voiceModelID;
-    
+
     /**
      * 방송 제목
      */
