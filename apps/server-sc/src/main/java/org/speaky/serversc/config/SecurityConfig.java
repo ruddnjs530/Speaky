0a21a7339aba4c22ecb3e6d4b690557e298a1624
sjkim1070 <sjkim1070@gmail.com>
2026-02-05 14:27:51 +0900

    fix(sc): disable wildcard cors origin for security

--- apps/server-sc/src/main/java/org/speaky/serversc/config/SecurityConfig.java
+++ apps/server-sc/src/main/java/org/speaky/serversc/config/SecurityConfig.java
@@ -84,8 +84,8 @@ public class SecurityConfig {
         // EC2 공인 도메인/IP (포트와 프로토콜 정확히 일치해야 함)
         configuration.addAllowedOrigin("http://i14b103.p.ssafy.io:5173"); 
         configuration.addAllowedOrigin("http://i14b103.p.ssafy.io");
-        // 개발 편의를 위해 패턴 허용 (모든 도메인 허용)
-        configuration.addAllowedOriginPattern("*");
+        // 개발 편의를 위해 패턴 허용 (모든 도메인 허용) - 보안상 주석 처리
+        // configuration.addAllowedOriginPattern("*");
 
         // 모든 HTTP 메서드 허용
         configuration.addAllowedMethod("*");
